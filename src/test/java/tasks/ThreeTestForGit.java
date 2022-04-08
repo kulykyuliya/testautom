997f7e2850770114b44b99e8768924f06d70ec0d
Nestors <nestors@Yuliya-Kulyk-MacBook-Pro.local>
2022-04-08 16:56:43 +0300

    ThreeBranch

---
+++ src/test/java/tasks/ThreeTestForGit.java
@@ -0,0 +1,23 @@
+package tasks;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ThreeTestForGit {
+    @Test
+    public void TestSecondBranch() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qa2.eatstreet.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//a[@id='menu-signin']")).click();
+        driver.findElement(By.xpath("//div[@id='uniqueid']")).click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("signin"));
+        driver.quit();
+    }
+
+}
