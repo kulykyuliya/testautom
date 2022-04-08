fcd133272897ff72a82f96bd5bd65c26de4afda2
Nestors <nestors@Yuliya-Kulyk-MacBook-Pro.local>
2022-04-08 16:31:09 +0300

    SecondBranch

--- src/test/java/tasks/task_first_EatStreet.java
+++ src/test/java/tasks/task_first_EatStreet.java
@@ -21,18 +21,18 @@ public class task_first_EatStreet {
         Assert.assertTrue(driver.getCurrentUrl().contains("eats"));
         driver.quit();
     }
-//    @Test
-//    public void intertopTest() {
-//        WebDriverManager.chromedriver().setup();
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://intertop.ua/ua/");
-//        driver.manage().window().maximize();
-//        driver.findElements(By.xpath("//span[@class='menu-item__name']")).get(6).click();
-//        sleep(3);
-//        driver.findElement(By.xpath("//div[@class='Button']")).click();
-//        driver.findElement(By.xpath("//span[contains(text(), 'Garnier')]")).click();
-//        driver.findElement(By.xpath("//span[contains(text(), 'Захист і гігієна')]")).click();
-//        Assert.assertTrue(driver.getCurrentUrl().contains("garnier"));
-//        driver.quit();
+    @Test
+    public void SingGoodle () {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get ("https://qa2.eatstreet.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//a[@id='menu-signin']")).click();
+        driver.findElement(By.xpath("//div[@id='uniqueid']")).click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("signin"));
+        driver.quit();
+
+    }
+
 }
 
